2fa69d7c93df2896c619e6746454e86648579578
geo-fh <georgesifarhat@gmail.com>
2025-05-28 02:35:35 +0300

    commit

---
+++ app/level1.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function Level1() {
+  return (
+    <View>
+      <Text style={[{color: 'white'}]}>Level1</Text>
+    </View>
+  )
+}
